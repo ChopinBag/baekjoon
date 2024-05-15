3813da9567178392feff98722d355c91a04dc2a9
ChopinBag <sw6129@pusan.ac.kr>
2024-05-15 14:07:20 +0900

    commit

---
+++ 11660_2.py
@@ -0,0 +1,20 @@
+## 또 시간초과!!!!!!!!!!!!!!
+tableSize, sumAttempt = map(int, input('표의 크기와 합의 횟수를 입력하시오: ').split())
+
+table = []
+for i in range(tableSize):
+    row = input().split()
+    row = [int(x) for x in row]
+    table.append(row)
+
+resultList = []
+
+for i in range(sumAttempt):
+    result = 0
+    x1, y1, x2, y2 = map(int, input().split())
+    for j in range(x2 - x1 + 1):
+        result += sum(table[x1 - 1 + j][y1-1 : y2])
+    resultList.append(result)
+
+for i in range(sumAttempt):
+    print(resultList[i])    
