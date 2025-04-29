9db86f77cded4209ebfbbf9e364ee78a7e271d3e
chopinbag <sw6129@pusan.ac.kr>
2025-04-29 15:10:57 +0900

    3190

--- Gold4/3190.cpp
+++ Gold4/3190.cpp
@@ -7,26 +7,30 @@ using namespace std;
 
 enum class Direction {
     RIGHT = 0,
+    DOWN,
     LEFT,
-    UP,
-    DOWN
+    UP
 };
 
 class Snake{
     public:
-        Snake(int r, int c, Direction d){
-            this->row = r;
-            this->col = c;
+        Snake() = default;
+        Snake(vector<vector<int>> &b,Direction d = Direction::RIGHT){
+            this->body = b;
             this->dir = d;
-        }
+        };
         void go_next(Direction d){
             
         }
+        void setup(vector<vector<int>> &b, int size){
+            b.assign(size, vector<int>(size, 0));
+            b[0][0] = 1; // 뱀 머리의 첫번째 위치
+        }
 
     private:
-        int row;
-        int col;
+        vector<vector<int>> body;
         Direction dir;
+        
 };
 
 
@@ -37,12 +41,16 @@ int main(){
     cout << "Enter the number of apples" << endl;
     cin >> apple_num;
     
-    vector<vector<int>> board[board_size][board_size];
+    vector<vector<int>> board;
+    board.assign(board_size, vector<int>(board_size, 0));
+    vector<vector<int>> body;
 
-    map<int, int> apple_location;
+    Snake s;
+    s.setup(body, board_size);
+
     for(int i=0;i<apple_num;++i){
         int row, col;
         cin >> row >> col;
-        apple_location[row] = col;
+        board[row][col] = 1;
     }
 }
