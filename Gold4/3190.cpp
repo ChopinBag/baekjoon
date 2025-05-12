d173abe602d27797ee26afe0dd819205fdd2ac83
chopinbag <sw6129@pusan.ac.kr>
2025-05-12 15:53:18 +0900

    3190 완료 !

--- Gold4/3190.cpp
+++ Gold4/3190.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
-#include <map>
 #include <string>
 
 using namespace std;
@@ -22,23 +21,22 @@ public:
     Snake(Direction d , vector<vector<int>> body, bool is_valid, vector<pair<int,int>> body_info, int board_size) 
     : dir_(d), body_(body), is_valid_(is_valid), body_info_(body_info), board_size_(board_size) {}
 
-    bool go_next(vector<vector<int>>& board) {
+    void go_next(vector<vector<int>>& board) {
         if(is_collide_wall()||is_collide_body()){
             is_valid_ = false;
-            return false;
+            return;
         }
-
         move_(board, direction_exchange(dir_).first, direction_exchange(dir_).second);
-        
-        time_++;
-        return true;
+    
+        // cout <<  "Direction:" << static_cast<int>(dir_) << " | head's location: " << body_info_.back().first << ", "<< body_info_.back().second << endl; //디버깅 용
+        return;
     }
 
     void change_direction(int isD) {
         if (isD==0) 
-            dir_ = static_cast<Direction>((static_cast<int>(dir_) + 1) % 4); // Clockwise
+            dir_ = static_cast<Direction>((static_cast<int>(dir_) + 1) % 4); // 시계방향
         else 
-            dir_ = static_cast<Direction>((static_cast<int>(dir_) + 3) % 4); // Counter-clockwise
+            dir_ = static_cast<Direction>((static_cast<int>(dir_) + 3) % 4); // 반시계방향
     }
 
 private:
@@ -61,16 +59,13 @@ private:
     }
     
     bool is_collide_wall() {
-        // 현재 머리 위치
         int next_row = body_info_.back().first + direction_exchange(dir_).first;
         int next_col = body_info_.back().second + direction_exchange(dir_).second;
     
-        // 다음 위치가 보드 경계를 벗어나는지 확인
         return next_row < 0 || next_row >= board_size_ || next_col < 0 || next_col >= board_size_;
     }
     
     bool is_collide_body() {
-        // 현재 머리 위치
         int next_row = body_info_.back().first + direction_exchange(dir_).first;
         int next_col = body_info_.back().second + direction_exchange(dir_).second;
     
@@ -79,8 +74,8 @@ private:
     }
     
     void move_(vector<vector<int>>&board, int row, int col){
-        if(board[body_info_.back().first][body_info_.back().second] == 1){ //사과가 있는 경우
-            board[body_info_.back().first][body_info_.back().second] = 0; // 사과를 삭제
+        if(board[body_info_.back().first+row][body_info_.back().second+col] == 1){ //사과가 있는 경우
+            board[body_info_.back().first+row][body_info_.back().second+col] = 0; // 사과를 삭제
             body_[body_info_.back().first+row][body_info_.back().second+col] = 1; // 몸을 늘린다.
             body_info_.push_back({body_info_.back().first+row, body_info_.back().second+col}); // 머리의 위치 수정
         } else { // 사과가 없는 경우
@@ -103,6 +98,7 @@ int main(){
     vector<vector<int>> board;
     board.assign(board_size, vector<int>(board_size, 0));
     vector<vector<int>> body = board;
+    body[0][0] = 1;
 
     Snake s(Direction::RIGHT, body, true, {{0,0}}, board_size);
     s.time_ = 0;
@@ -110,8 +106,7 @@ int main(){
     for(int i=0;i<apple_num;++i){
         int row, col;
         // cout << "Enter the location of the apple #"<< i << endl; // 디버깅 용
-        do{cin >> row >> col;}
-        while(row > board_size || col > board_size || row < 1 || col < 1); // 입력값 검증
+        cin >> row >> col;
         board[row - 1][col - 1] = 1;
         // 사과의 위치 정보 설정
     }
@@ -128,8 +123,7 @@ int main(){
         // cout << "Enter the direction information #"<< i << endl;// 디버깅 용
         cin >> time >> change;
         pair<int,int> temp_pair;
-        if(i!=0) temp_pair.second = time - change_list.back().second;
-        else temp_pair.second = time;
+        temp_pair.second = time;
 
         if(change == "D") temp_pair.first = 0; 
         // 0 은 시계방향 -> Direction++
@@ -137,15 +131,19 @@ int main(){
         // 1 은 반시계방향 -> Direction--
         change_list.push_back(temp_pair);
     }
-
-    for(int i=0;i<direction_change;++i){
-        int j=0;
-        while(j++ < change_list[i].second && s.go_next(board)){;}
-        if(!s.is_valid_) break;
+// s.time_ 이 change_list[i].second 보다 작으면 go_next, is_valid_가 false 일 시에 즉시 종료. change_list[i].second == s.time_ 이면 i++
+// s.time_ 증가도 다루어야함
+    int c1 = 0;     
+    while(s.is_valid_){
         
-        s.change_direction(change_list[i].first);
+        s.go_next(board);
+        s.time_++;
+
+        if (change_list[c1].second == s.time_){
+            s.change_direction(change_list[c1].first);
+            if (c1 < change_list.size()) c1++; // c1은 change_list 크기 만큼만 증가.
+        }
     }
-    while(s.go_next(board)){;}
     
     cout << s.time_ << endl;
 }
